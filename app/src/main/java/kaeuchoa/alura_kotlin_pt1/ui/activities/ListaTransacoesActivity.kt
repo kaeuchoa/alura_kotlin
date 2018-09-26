92dc8bc2ea67ab2f0d77084ad3cfebb4cda1f15d
kaeuchoa <kaeuchoa@hotmail.com>
2018-09-26 10:07:22 -0300

    feat: inclui os arquivos de resources

--- app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
@@ -2,13 +2,22 @@ package kaeuchoa.alura_kotlin_pt1.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import kaeuchoa.alura_kotlin_pt1.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida R$ 20,50", "Economia R$ 100,00")
+
+        ArrayAdapter(this,android.R.layout.simple_list_item_1,transacoes)
+
+
+
     }
 
 }
