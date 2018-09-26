98bd20afb9a6fd32ced522ef4aea4e54f4bb6c0e
kaeuchoa <kaeuchoa@hotmail.com>
2018-09-26 09:56:08 -0300

    commit inicial com a activity base

---
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
@@ -0,0 +1,14 @@
+package kaeuchoa.alura_kotlin_pt1.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kaeuchoa.alura_kotlin_pt1.R
+
+class ListaTransacoesActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
