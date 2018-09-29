87539134448e2b1aab77f48f6345671197f09307
kaeuchoa <kaeuchoa@hotmail.com>
2018-09-29 10:47:14 -0300

    feat: adiciona o modelo de Transação e modifica o adapter para mostrar os dados da estrutura da classe

--- app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
@@ -3,8 +3,11 @@ package kaeuchoa.alura_kotlin_pt1.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kaeuchoa.alura_kotlin_pt1.R
+import kaeuchoa.alura_kotlin_pt1.models.Transacao
 import kaeuchoa.alura_kotlin_pt1.ui.adapters.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import java.math.BigDecimal
+import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity(){
 
@@ -12,7 +15,8 @@ class ListaTransacoesActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf("Comida R$ 20,50", "Economia R$ 100,00")
+        val transacoes = listOf(Transacao(BigDecimal(20.50), "Comida", Calendar.getInstance()),
+                Transacao(BigDecimal(100),"Economia", Calendar.getInstance()))
 
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes,this)
 
--- app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/adapters/ListaTransacoesAdapter.kt
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/adapters/ListaTransacoesAdapter.kt
@@ -6,17 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import kaeuchoa.alura_kotlin_pt1.R
+import kaeuchoa.alura_kotlin_pt1.models.Transacao
+import kotlinx.android.synthetic.main.transacao_item.view.*
 
-class ListaTransacoesAdapter (listaTransacoes : List<String>, context: Context) : BaseAdapter(){
+class ListaTransacoesAdapter (listaTransacoes: List<Transacao>, context: Context) : BaseAdapter(){
 
     private val listaTransacoes = listaTransacoes
     private val context = context
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        return LayoutInflater.from(context).inflate(R.layout.transacao_item,parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
+        val transacao : Transacao = listaTransacoes[position]
+
+        view.transacao_data.text = transacao.getData().toString()
+        view.transacao_categoria.text = transacao.getCategoria()
+        view.transacao_valor.text = transacao.getValor().toString()
+
+
+        return view
     }
 
-    override fun getItem(position: Int): Any {
+    override fun getItem(position: Int): Transacao {
         return listaTransacoes[position]
     }
 
