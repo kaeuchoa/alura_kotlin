5c2e3e200aa3b910e3992d8a274273c1212e3817
kaeuchoa <kaeuchoa@hotmail.com>
2018-09-29 11:47:30 -0300

    refactor: modifica a classe Transacao para se adequar ao uso de properties do kotlin

--- app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/adapters/ListaTransacoesAdapter.kt
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/adapters/ListaTransacoesAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter
 import kaeuchoa.alura_kotlin_pt1.R
 import kaeuchoa.alura_kotlin_pt1.models.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
+import java.text.SimpleDateFormat
 
 class ListaTransacoesAdapter (listaTransacoes: List<Transacao>, context: Context) : BaseAdapter(){
 
@@ -18,9 +19,13 @@ class ListaTransacoesAdapter (listaTransacoes: List<Transacao>, context: Context
         val view = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
         val transacao : Transacao = listaTransacoes[position]
 
-        view.transacao_data.text = transacao.getData().toString()
-        view.transacao_categoria.text = transacao.getCategoria()
-        view.transacao_valor.text = transacao.getValor().toString()
+        val formatoData = "dd/MM/yyyy"
+        val formatDate = SimpleDateFormat(formatoData)
+        val dataFormatada = formatDate.format(transacao.data.time)
+
+        view.transacao_data.text = dataFormatada
+        view.transacao_categoria.text = transacao.categoria
+        view.transacao_valor.text = transacao.valor.toString()
 
 
         return view
