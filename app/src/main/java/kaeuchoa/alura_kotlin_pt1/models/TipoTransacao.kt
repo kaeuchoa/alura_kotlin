0046b0053ea4c9c96bca7501036b26714d6d5bc2
kaeuchoa <kaeuchoa@hotmail.com>
2018-09-29 12:35:27 -0300

    feat/refactor: adiciona um enum para indicar o tipo da transação e utiliza Named Parameters para sobrecarga de construtores de Transacao

---
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/models/TipoTransacao.kt
@@ -0,0 +1,5 @@
+package kaeuchoa.alura_kotlin_pt1.models
+
+enum class TipoTransacao {
+    RECEITA, DESPESA
+}
--- app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
+++ app/src/main/java/kaeuchoa/alura_kotlin_pt1/ui/activities/ListaTransacoesActivity.kt
@@ -3,11 +3,11 @@ package kaeuchoa.alura_kotlin_pt1.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kaeuchoa.alura_kotlin_pt1.R
+import kaeuchoa.alura_kotlin_pt1.models.TipoTransacao
 import kaeuchoa.alura_kotlin_pt1.models.Transacao
 import kaeuchoa.alura_kotlin_pt1.ui.adapters.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import java.math.BigDecimal
-import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity(){
 
@@ -15,8 +15,8 @@ class ListaTransacoesActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(Transacao(BigDecimal(20.50), "Comida", Calendar.getInstance()),
-                Transacao(BigDecimal(100),"Economia", Calendar.getInstance()))
+        val transacoes = listOf(Transacao(valor = BigDecimal(20.50), tipo =TipoTransacao.DESPESA),
+                Transacao(valor = BigDecimal(100),categoria = "Economia",tipo = TipoTransacao.RECEITA))
 
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes,this)
 
